55d7c4e8a639e61c7f23a380befce0607034bb52
josephquested <josephquested@gmail.com>
2017-03-30 22:26:28 +1300

    state machine up and running for movement animation

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+	StateMachine sm;
+	Animator animator;
+
+	float horizontal;
+	float vertical;
+
+	void Start ()
+	{
+		sm = GetComponent<StateMachine>();
+		animator = GetComponent<Animator>();
+	}
+
+	void Update ()
+	{
+		UpdateMovement();
+		UpdateAnimator();
+	}
+
+	void UpdateMovement ()
+	{
+		if (sm.state == States.Idle)
+		{
+			print("I am idle");
+		}
+	}
+
+	void UpdateAnimator ()
+	{
+		animator.SetBool("moving", sm.state == States.Moving);
+	}
+
+	public void ReceiveInput (float h, float v)
+	{
+		horizontal = h;
+		vertical = v;
+	}
+}
---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour {
+
+	Movement movement;
+
+	void Start ()
+	{
+		movement = GetComponent<Movement>();
+	}
+
+	void Update ()
+	{
+		UpdateMovement();
+	}
+
+	void UpdateMovement ()
+	{
+		movement.ReceiveInput(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+	}
+}
