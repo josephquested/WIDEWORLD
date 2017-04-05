107710caa2388e64b1fde7597c5d185db3186ccb
Joseph Quested <josephquested@gmail.com>
2017-04-05 21:38:48 +1200

    intitial knife.cs workl

---
+++ Assets/Scripts/Blade.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blade : Projectile {
+
+	public int damage;
+
+	public float knockback;
+
+	public override void OnHittableEnter (Hittable hittable)
+	{
+		hittable.ReceiveDamage(damage);
+		hittable.ReceiveKnockback(GetComponent<Rigidbody2D>().velocity, knockback);
+		Destroy(gameObject);
+	}
+}
---
+++ Assets/Scripts/Knife.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knife : Weapon {
+
+	Rigidbody2D rb;
+
+	public float thrust;
+
+	void Start ()
+	{
+		rb = transform.parent.GetComponent<Rigidbody2D>();
+	}
+
+	public override void OnDown ()
+	{
+		if (down)
+		{
+			if (sm.AttemptTransition(States.Attacking))
+			{
+				Fire();
+				Thrust();
+			}
+		}
+	}
+
+	void Fire ()
+	{
+		GameObject knife = Instantiate(prefab, transform.position, transform.rotation);
+		knife.GetComponent<Blade>().SetParent(transform.parent);
+		audioSource.Play();
+	}
+
+	void Thrust ()
+	{
+		rb.AddForce(GetDirection() * thrust, ForceMode2D.Impulse);
+	}
+}
